5194bbb631f45da12e66f9662da4ceb488f0609e
TonyJem <62282734+TonyJem@users.noreply.github.com>
2021-02-23 23:13:04 +0200

    Connect TopView with XIB

--- ChatTableView/ChatTableView/View/TopView.swift
+++ ChatTableView/ChatTableView/View/TopView.swift
@@ -10,6 +10,24 @@ class TopView: UIView {
     @IBOutlet var likeCountLabel: UILabel!
     @IBOutlet var textLabel: UILabel!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     @IBAction func likButtonTapped(_ sender: Any) {
+    }
+}
+
+private extension TopView {
+    func commonInit() {
+        guard let view = Bundle.main.loadNibNamed(String(describing: TopView.self),
+                                                  owner: self,
+                                                  options: nil)?.first as? UIView else { return }
+        view.frame = self.bounds
+        addSubview(view)
     }
 }
