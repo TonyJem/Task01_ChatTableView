b680c21c4c802acde4afd52779e35d151527263a
TonyJem <62282734+TonyJem@users.noreply.github.com>
2021-02-23 22:51:17 +0200

    Finish Creating TopView.XIB

--- ChatTableView/ChatTableView/View/TopView.swift
+++ ChatTableView/ChatTableView/View/TopView.swift
@@ -2,4 +2,14 @@ import UIKit
 
 class TopView: UIView {
     
+    @IBOutlet var userImageView: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var likeCountLabel: UILabel!
+    @IBOutlet var textLabel: UILabel!
+    
+    @IBAction func likButtonTapped(_ sender: Any) {
+    }
 }
