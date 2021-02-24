7744f2e2f2cdadd1a27a70637e4a782938072936
TonyJem <62282734+TonyJem@users.noreply.github.com>
2021-02-24 03:37:54 +0200

    Add "dateFormatter" and use it for "dateLabel"

--- ChatTableView/ChatTableView/View/SimpleTableViewCell.swift
+++ ChatTableView/ChatTableView/View/SimpleTableViewCell.swift
@@ -1,8 +1,12 @@
 import UIKit
 
 class SimpleTableViewCell: UITableViewCell {
-    @IBOutlet var userImageView: UIImageView!
     
+    struct Constants {
+        static let timeFormat = "hh:mm a"
+    }
+    
+    @IBOutlet var userImageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
@@ -14,6 +18,11 @@ class SimpleTableViewCell: UITableViewCell {
         nameLabel.text = model.name
         likeCountLabel.text = "\(model.likeCount ?? 0)"
         contentTextLabel.text = model.text
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = Constants.timeFormat
+        
+        dateLabel.text = dateFormatter.string(from: model.date)
     }
     
     @IBAction func likeButtonTapped(_ sender: Any) {
