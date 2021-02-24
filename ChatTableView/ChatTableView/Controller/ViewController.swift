fd300eb048d7a3294a1e866cabfd9833dd7192c4
TonyJem <62282734+TonyJem@users.noreply.github.com>
2021-02-24 05:30:27 +0200

    Minor adjustments in XIBs

--- ChatTableView/ChatTableView/Controller/ViewController.swift
+++ ChatTableView/ChatTableView/Controller/ViewController.swift
@@ -6,9 +6,9 @@ class ViewController: UIViewController {
     
 //    TODO: testData
     let list: [User] = [
-        User(name: "Test1", date: Date(), photo: UIImage(named: "avatar"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, risus at consectetur malesuada, justo magna vehicula purus, sit amet commodo lectus neque vel lacus. Praesent venenatis enim mi. Phasellus semper bibendum commodo. Morbi leo risus, vestibulum id interdum interdum, euismod interdum est. ", likeCount: 12, picture: nil),
-        User(name: "Test2", date: Date(), photo: UIImage(named: "avatar"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, risus at consectetur malesuada, justo magna vehicula purus, sit amet commodo lectus neque vel lacus. Praesent venenatis enim mi. Phasellus semper bibendum commodo. Morbi leo risus, vestibulum id interdum interdum, euismod interdum est. ", likeCount: 12, picture: UIImage(named: "boxedwater")),
-        User(name: "Test3", date: Date(), photo: UIImage(named: "avatar"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, risus at consectetur malesuada, justo magna vehicula purus, sit amet commodo lectus neque vel lacus. Praesent venenatis enim mi. Phasellus semper bibendum commodo. Morbi leo risus, vestibulum id interdum interdum, euismod interdum est. ", likeCount: 12, picture: nil)
+        User(name: "Test1", date: Date(), photo: UIImage(named: "avatar"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", likeCount: 11, picture: nil),
+        User(name: "Test2", date: Date(), photo: UIImage(named: "avatar"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, risus at consectetur malesuada, justo magna vehicula purus, sit amet commodo lectus neque vel lacus. Praesent venenatis enim mi. Phasellus semper bibendum commodo. Morbi leo risus, vestibulum id interdum interdum, euismod interdum est. ", likeCount: 22, picture: UIImage(named: "boxedwater")),
+        User(name: "Test3", date: Date(), photo: UIImage(named: "avatar"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, risus at consectetur malesuada, justo magna vehicula purus, sit amet commodo lectus neque vel lacus. Praesent venenatis enim mi. Phasellus semper bibendum commodo. Morbi leo risus, vestibulum id interdum interdum, euismod interdum est. ", likeCount: 88, picture: nil)
     ]
     
     override func viewDidLoad() {
