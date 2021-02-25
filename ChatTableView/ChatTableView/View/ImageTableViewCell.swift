56927a21395e39c4899b7fa626e75bd72c5ab5ba
TonyJem <a.jemanov@gmail.com>
2021-02-25 20:57:13 +0200

    Wrap "postPicture" in container

--- ChatTableView/ChatTableView/View/ImageTableViewCell.swift
+++ ChatTableView/ChatTableView/View/ImageTableViewCell.swift
@@ -2,15 +2,25 @@ import UIKit
 
 class ImageTableViewCell: SimpleTableViewCell {
     private struct Constants {
-        static let pictureRadius: CGFloat = 15
+        static let pictureRadius: CGFloat = 10
     }
     
-    @IBOutlet var bottomImageView: UIImageView!
+    @IBOutlet private var postImageContainer: UIView! {
+        didSet {
+            postImageContainer.layer.cornerRadius = 10
+            postImageContainer.layer.shadowColor = UIColor.darkGray.cgColor
+            postImageContainer.layer.shadowOffset = CGSize(width: 5, height: 5)
+            postImageContainer.layer.shadowRadius = 5
+            postImageContainer.layer.shadowOpacity = 0.2
+        }
+    }
+    @IBOutlet private var postImageView: UIImageView!
     
     override func fill(model: User) {
         super.fill(model: model)
         
-        bottomImageView.image = model.picture
-        bottomImageView.layer.cornerRadius = Constants.pictureRadius
+        postImageView.image = model.picture
+        postImageView.layer.cornerRadius = Constants.pictureRadius
+        postImageView.clipsToBounds = true
     }
 }
