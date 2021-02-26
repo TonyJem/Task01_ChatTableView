02c1bc7feb7035a88273e47aca823ec35bb64df8
TonyJem <a.jemanov@gmail.com>
2021-02-26 13:38:24 +0200

    Refactor: Remove unnecessary class from project

--- ChatTableView/ChatTableView/view/TopView.swift
+++ ChatTableView/ChatTableView/view/TopView.swift
@@ -1,24 +1,22 @@
 import UIKit
-
-class TopView: UIView {
-    //        TODO: код есть - использования кода нет
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        commonInit()
-    }
-}
-
-private extension TopView {
-    func commonInit() {
-        guard let view = Bundle.main.loadNibNamed(String(describing: TopView.self),
-                                                  owner: self,
-                                                  options: nil)?.first as? UIView else { return }
-        view.frame = self.bounds
-        addSubview(view)
-    }
-}
+//
+//class TopView: UIView {
+//    //        TODO: код есть - использования кода нет
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        commonInit()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        commonInit()
+//    }
+//}
+//
+//private extension TopView {
+//    func commonInit() {
+//        guard let view = Bundle.main.loadNibNamed(String(describing: TopView.self), owner: self, options: nil)?.first as? UIView else { return }
+//        view.frame = self.bounds
+//        addSubview(view)
+//    }
+//}
