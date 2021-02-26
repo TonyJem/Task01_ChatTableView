f63accc5ebf4963e94fec674b239c0358048768c
TonyJem <a.jemanov@gmail.com>
2021-02-26 13:32:38 +0200

    Refactor: Remove unnecessary self from code

--- ChatTableView/ChatTableView/settings/UIView+Extension.swift
+++ ChatTableView/ChatTableView/settings/UIView+Extension.swift
@@ -15,14 +15,11 @@ extension UIView {
     }
     
     //    TODO: 1. перенос на новую строку только если не влазит
-    //    2. let layer = self.layer не нужно выделять переменную работай напрямую с self.layer
     func setShadow(offsetSize: CGSize = CGSize(width: 5, height: 5),
                    color: CGColor = Colors.shadowColor,
                    opacity: Float = 0.4,
                    radius: CGFloat = 5)
     {
-        let layer = self.layer
-        
         layer.shadowOffset = offsetSize
         layer.shadowColor = color
         layer.shadowOpacity = opacity
